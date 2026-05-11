69b2d7cc58609b28467e805dcbc7cd0d5024b420
exilio1 <167363156+exilio1@users.noreply.github.com>
2026-05-11 11:40:20 -0500

    agrego los archivos init para que Python reconozca los paquetes del proyecto

---
+++ app/models/__init__.py
@@ -0,0 +1,6 @@
+from app.models.usuario_model import UsuarioModel
+from app.models.empleado_model import EmpleadoModel
+from app.models.cliente_model import ClienteModel
+from app.models.cita_model import CitaModel
+from app.models.pago_model import PagoModel
+from app.models.gasto_model import GastoModel
